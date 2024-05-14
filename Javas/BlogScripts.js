93c85c86bbcb44a805300c5fc45f55c0d4ccc2d8
Werner De Juan <wernerdjuan@gmail.com>
2024-05-14 14:45:58 +0200

    Update BlogScripts.js

--- Javas/BlogScripts.js
+++ Javas/BlogScripts.js
@@ -1,6 +1,4 @@
 /*
 document.body.innerHTML = document.body.innerHTML + ("Last modified: " + document.lastModified);
 */
-Path file = Paths.get("https://raw.githubusercontent.com/WernerDJ/sample_website/main/pages/BlogPost_Inteligencia-libertad.html");
-BasicFileAttributes attr = Files.readAttributes(file, BasicFileAttributes.class);
-document.body.innerHTML = document.body.innerHTML + ("lastModifiedTime: " + attr.lastModifiedTime());
+
