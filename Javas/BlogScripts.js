f2636be9cb5bda0b3a2f4464a74a026141ddaf3a
Werner De Juan <wernerdjuan@gmail.com>
2024-05-15 12:44:02 +0200

    Update BlogScripts.js

--- Javas/BlogScripts.js
+++ Javas/BlogScripts.js
@@ -1,2 +1,4 @@
 document.body.innerHTML = document.body.innerHTML + ("Last modified: " + document.lastModified);
-
+function changeBackgroundColor(newColor) {
+document.body.style.backgroundColor = newColor;
+}
