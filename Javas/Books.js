8f6c281a92a96f0da778a9f731cff2310f08af03
Werner De Juan <wernerdjuan@gmail.com>
2024-05-18 07:43:14 +0200

    Update Books.js

--- Javas/Books.js
+++ Javas/Books.js
@@ -3,7 +3,7 @@ const images = document.getElementsByTagName('img');
 // Loop through the images
 for (const image of images) {
 // Listen for the right click event on each image
-image.addEventListener('mouseenter', function(e) {
+image.addEventListener('mouseover', function(e) {
 // Prevent the context menu from appearing
 //e.preventDefault();
 // Toggle the full-size class on the image
